55cf6278193eb9cf999d0d7424c3cf640909ec88
PartnerNET <business@partnernetsoftware.com>
2020-09-30 14:23:04 +0800

    cleanup py

--- main3.py
+++ main3.py
@@ -34,7 +34,8 @@ Loading caffe models at once
 
 proto = "VS-Python/Models/mobilenet_v2_deploy.prototxt"
 model = "../d/mobilenet_v2.caffemodel"
-caffe.set_mode_gpu()
+#caffe.set_mode_gpu()
+caffe.set_mode_cpu()
 net = caffe.Net(proto, model, caffe.TEST)
 
 transformer = caffe.io.Transformer({'data':net.blobs['data'].data.shape})
@@ -58,9 +59,41 @@ net1.blobs['data'].reshape(1, 3, 227, 227)
 print ("Caffe Model for Memorability Prediction Loaded")
 
 class Main: 
+    def mem_calculation(frame1):
+        start_time1 = time.time()
+        resized_image = caffe.io.resize_image(frame1,[227,227])
+        net1.blobs['data'].data[...] = transformer1.preprocess('data', resized_image)
+        value = net1.forward()
+        value = value['fc8-euclidean']
+        end_time1 = time.time()
+        execution_time1 = end_time1 - start_time1
+        #print ("*********** \t Execution Time in Memobarility = ", execution_time1, " secs \t***********")
+        return value[0][0]
+    def shot_segment(frame1,frame2):
+        #print ("Inside segment function")
+        start_time1 = time.time()
+        resized_image1 = caffe.io.resize_image(frame1,[224,224])
+        resized_image2 = caffe.io.resize_image(frame2,[224,224])
+        
+        
+#        transformer.set_mean('data',img_mean)
+        net.blobs['data'].reshape(1, 3, 224, 224)
+        net.blobs['data'].data[...] = transformer.preprocess('data', resized_image1)
+        net.forward()
+        features1 = net.blobs['fc7'].data[0].reshape(1,1000)
+        features1 = np.array(features1)
+        net.blobs['data'].data[...] = transformer.preprocess('data', resized_image2)
+        net.forward()
+        features2 = net.blobs['fc7'].data[0].reshape(1,1000)
+        features2 = np.array(features2)
+        
+        #t = caclulate_distance(self,features1,features2)
+        end_time1 = time.time()
+        execution_time1 = end_time1 - start_time1
+        #print ("*********** \t Execution Time in shot segmentation= ", execution_time1, " secs \t***********")
+        return euclidean_distances(features1,features2)
     
     def main():
-        #print ("Inside main function")
         capture = cv2.VideoCapture(video_path)
         total_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))#getting total number of frames
         fps = int(capture.get(cv2.CAP_PROP_FPS))#getting frame rate
@@ -81,11 +114,7 @@ class Main:
             ttt = ttt + 1
             ret2, frame2 = capture.read()
             if ret2 is True:
-#            capture.release()
-
-                #shot_segmentation=Shot_segmentation()
-                #distt = shot_segmentation.segment(frame1,frame2)
-                distt = Shot_segmentation.segment(frame1,frame2) # time 0.5
+                distt = Main.shot_segment(frame1,frame2) # time 0.5
 
                 print ('Processing ... ', ttt, ', of ', total_frames, 'with distt=',distt)
                 if distt >= 40000:#{ different images , 25x4
@@ -95,7 +124,6 @@ class Main:
                     if cols > 0:
                         max_index = m_scores[0].argmax()
                         keyframe_number = int(m_scores[1][max_index])
-                        # CAP_PROP_POS_MSEC in microsecond
                         keyframe = capture.set(cv2.CAP_PROP_POS_MSEC, keyframe_number)
     #                        print (keyframe_number , " \t########")
                         temp, keyframe = capture.read()
@@ -108,10 +136,8 @@ class Main:
                     m_scores.append([])
                     #}
                 else:#{ same images
-                    #print ("Similar images= " , distt , "\t" , counter)
-                    #memorability_prediction=Memorability_Prediction()
-                    #m_value = memorability_prediction.mem_calculation(frame1)
-                    m_value = Memorability_Prediction.mem_calculation(frame1)  # time 0.15
+                    print ("Similar images= " , distt , "\t" , counter)
+                    m_value = Main.mem_calculation(frame1)  # time 0.15
 
     #                scores[index][0] = m_value
                     m_scores[0].append(m_value)
@@ -134,51 +160,8 @@ class Main:
             #cv2.waitKey(2)
         
         capture.release()  
-#        cv2.destroyAllWindows()
 
     
-#############################################################################
-class Shot_segmentation:
-    #def segment(self,frame1,frame2):
-    def segment(frame1,frame2):
-        #print ("Inside segment function")
-        start_time1 = time.time()
-        resized_image1 = caffe.io.resize_image(frame1,[224,224])
-        resized_image2 = caffe.io.resize_image(frame2,[224,224])
-        
-        
-#        transformer.set_mean('data',img_mean)
-        net.blobs['data'].reshape(1, 3, 224, 224)
-        net.blobs['data'].data[...] = transformer.preprocess('data', resized_image1)
-        net.forward()
-        features1 = net.blobs['fc7'].data[0].reshape(1,1000)
-        features1 = np.array(features1)
-        net.blobs['data'].data[...] = transformer.preprocess('data', resized_image2)
-        net.forward()
-        features2 = net.blobs['fc7'].data[0].reshape(1,1000)
-        features2 = np.array(features2)
-        
-        #t = caclulate_distance(self,features1,features2)
-        end_time1 = time.time()
-        execution_time1 = end_time1 - start_time1
-        print ("*********** \t Execution Time in shot segmentation= ", execution_time1, " secs \t***********")
-        return euclidean_distances(features1,features2)
-    
-class Memorability_Prediction:
-    #def mem_calculation(self,frame1):
-    def mem_calculation(frame1):
-        #print ("Inside mem_calculation function")
-        start_time1 = time.time()
-        resized_image = caffe.io.resize_image(frame1,[227,227])
-        net1.blobs['data'].data[...] = transformer1.preprocess('data', resized_image)
-        value = net1.forward()
-        value = value['fc8-euclidean']
-        end_time1 = time.time()
-        execution_time1 = end_time1 - start_time1
-        print ("*********** \t Execution Time in Memobarility = ", execution_time1, " secs \t***********")
-        return value[0][0]
-    
-
 if __name__ == '__main__':
     Main.main()
 
--- szubdi.md
+++ szubdi.md
@@ -8,3 +8,28 @@ sudo docker build . -t szubdi/caffe:latest
 sh run.sh ../d/test.mp4
 
 ```
+
+local env
+
+```
+
+#sudo sed -i 's/http:\/\/archive.ubuntu.com\/ubuntu\//http:\/\/mirrors.aliyun.com\/ubuntu\//g' /etc/apt/sources.list && apt-get update
+cat << EEE | sudo bash
+sed -i 's/http:\/\/.*archive.ubuntu.com\/ubuntu\//http:\/\/mirrors.aliyun.com\/ubuntu\//g' /etc/apt/sources.list 
+apt update
+apt install -y python3-venv cmake caffe-cuda
+EEE
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip -V
+python -V
+
+
+pip install scikit-build opencv-python sklearn -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+pip list
+
+
+```
